2d19f06a8e8284ecf288ae110f03642c5b69c079
Nexter18 <54656466+Nexter18@users.noreply.github.com>
2019-09-29 23:16:46 -0600

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,3 +1,4 @@
+#This was another way of triggering creation and saving profiles
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
